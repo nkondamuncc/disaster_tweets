fbe3b87883999859da4db2cb2fdb56084a943efd
nkondamuncc <129068001+nkondamuncc@users.noreply.github.com>
2023-05-06 09:45:07 -0400

    Update 03_Word_cloud.py

--- pages/03_Word_cloud.py
+++ pages/03_Word_cloud.py
@@ -46,7 +46,6 @@ def load_data():
 def show_wc(text,cmap):
     bc = st.get_option('theme.backgroundColor')
     mask = np.array(Image.open(r'data/10wmt-superJumbo-v4.jpg'))
-    st.write(bc)
     wordcloud = WordCloud( max_font_size=200,width=2000, height=1000,max_words=150,collocations=False,stopwords=stop_words,background_color='#002b36', colormap=cmap, mask=mask).generate(text)
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
